b8c7d7cb5415552aba40a69930a0f759e8c70302
고재훈 <works.hoon@gmail.com>
2019-03-05 15:52:07 +0900

    파일 정리

--- 1장/5_제어흐름분석.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function projectStatus(x) {
-    if (typeof x === 'string') { // x는 string 이거나 number 타입
-        x = 10;
-    }
-    return x; // number 타입을 변환
-}
-console.log(projectStatus('10'));
-//# sourceMappingURL=5_제어흐름분석.js.map
--- 1장/8_인터페이스.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Earth = /** @class */ (function () {
-    function Earth() {
-    }
-    return Earth;
-}());
-var planet = new Earth();
-//# sourceMappingURL=8_인터페이스.js.map
