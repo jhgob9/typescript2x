bde5e3d7bf081bd04c00af2c427146471fd8dd85
고재훈 <jhgo@bitnine.net>
2019-03-19 10:17:16 +0900

    5장 트렐로

--- SampleTrelloApplication/src/app/app.module.ts
+++ SampleTrelloApplication/src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { BoardComponent } from './board/board.component';
 
 import { TrelloService } from './service/trello.service';
+import { TaskComponent } from './task/task.component';
+import { SubtaskComponent } from './subtask/subtask.component';
+
+const appRoutes: Routes = [
+	{ path: 'board/:id', component: BoardComponent, pathMatch: 'full' },
+
+	{ path: '', component: HomepageComponent },
+];
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		HomepageComponent
+		HomepageComponent,
+		BoardComponent,
+		TaskComponent,
+		SubtaskComponent
 	],
 	imports: [
 		BrowserModule,
-		HttpClientModule
+		HttpClientModule,
+		FormsModule,
+		RouterModule.forRoot(appRoutes)
 	],
 	providers: [TrelloService],
 	bootstrap: [AppComponent]
